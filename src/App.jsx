a7e4a4d253600e67a7ab3faa2c4bacd81659db5a
Vishal Vivare <96101110+vishalvivare@users.noreply.github.com>
2022-05-01 10:21:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import "./App.css";
+import { Inventory } from "./components/Inventory";
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Inventory</h1>
+      <div>
+      <Inventory />
+      </div>
+    </div>
+  );
+}
+
+export default App;
